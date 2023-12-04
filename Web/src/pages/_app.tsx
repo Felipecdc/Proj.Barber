7ed22ed434e938b00dc0de88f956306968b08f93
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-12-03 22:58:47 -0300

    Add files via upload

---
+++ Web/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../../styles/global.scss';
+import { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { AuthProvider } from '../contexts/AuthContext';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return( 
+    <AuthProvider>
+      <Component {...pageProps} />
+      <ToastContainer autoClose={3000} />
+    </AuthProvider>
+  )
+}
+ 
---
+++ Web/src/utils/canSSRGuest.ts
@@ -0,0 +1,20 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { parseCookies } from 'nookies';
+
+export function canSSRGuest<p>(fn: GetServerSideProps<p>){
+    return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<p>> => {
+
+        const cookies = parseCookies(ctx)
+
+        if(cookies["@barbearia.token"]){
+            return {
+                redirect: {
+                    destination: "/dashboard",
+                    permanent: false,
+                }
+            }
+        }
+
+        return await fn(ctx);
+    }
+}
